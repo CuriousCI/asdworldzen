8e7cd17d1ca98c034f7233b2b3c6b25c69353fa6
Ionut Cicio <ionutcicio.dev@gmail.com>
2023-04-18 07:20:08 +0200

    updated sitemap with subdomain

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
     export let title: string = "",
-        icon: string = "",
         href: string = "https://app.worldzen.it",
         background: string = "";
 </script>
 
 <section class="w-full min-h-min p-10 {background}">
     <a {href}>
-        <h2
-            class="text-2xl sm:text-4xl md:text-5xl mb-10 font-bold flex items-center gap-2"
-        >
+        <h2 class="text-2xl sm:text-4xl md:text-5xl mb-10 font-bold">
             {title}
-            <i
-                class="material-symbols-outlined text-2xl sm:text-4xl md:text-5xl"
-            >
-                {icon}
-            </i>
         </h2>
     </a>
     <div
